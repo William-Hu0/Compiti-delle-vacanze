07377721a99857e5674ee8aa06cf00fb38c0895f
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-04-25 15:57:35 +0000

    ciao

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import pandas as pd
 app = Flask(__name__) 
-lista=[1,2,3,4,5]
+
+crediti=100
 
 @app.route("/")
 def index():
-    return render_template("index.html", lista=lista)
+    df = pd.read_csv("profilo.csv")
+    dati = df.iloc[0].to_dict()
+    return render_template("index.html", crediti=crediti, dati=dati)
 
-@app.route("/profilo")
-def profilo():
 
-    return "Questa è la pagina profilo"
-
-@app.route("/aggiungi",methods=["POST"])
-def aggiungi():
-    elemento = request.form['elemento']
-    if elemento:
-        lista.append(elemento)
-    return redirect(url_for('index'))
-    return render_template("index.html")
 
 
 
